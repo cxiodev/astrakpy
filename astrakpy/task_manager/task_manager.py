e4b1a0460a57e2fce00550a4c3ef2e4cfe55d2c3
newpotsdev <triedofficial@gmail.com>
2020-02-17 19:30:40 +0100

    Better task manager

--- astrakpy/task_manager/task_manager.py
+++ astrakpy/task_manager/task_manager.py
@@ -15,5 +15,13 @@ class TaskManager:
             task.run(self.loop)
 
     def run(self):
+        if self.tasks.__len__() <= 0:
+            raise ValueError("You have no tasks created.")
         self._run_tasks()
         self.loop.run_forever()
+
+    def get_by_name(self, name):
+        for task in self.tasks:
+            if task.name == name:
+                return name
+        raise LookupError(f"No task found with name {name}.")
